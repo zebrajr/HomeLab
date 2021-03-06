ae3eddc13862f6f02a196123c1555402f38dbb10
Carlos Sousa <csousa90@gmail.com>
2021-03-06 19:39:44 +0100

    Updated file location

--- curiosity/rsync/sync-from-opportunity.sh
+++ curiosity/rsync/sync-from-opportunity.sh
@@ -5,8 +5,6 @@
 # Run Every Day / If not executed wait 15 minutes after system boot / Unique Name / Script Location
 # 1  15  syncFromOpportunity.daily  /bin/sh /home/rsync/sync-from-opportunity.sh
 
+
 . ./home/rsync/config.sh
-
-
-
 rsync --progress -avz -e "ssh -i ${SOURCEKEY}" $TARGETUSER@$TARGETIP:$TARGETFOLDER $SOURCEFOLDER
