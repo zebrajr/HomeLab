f96bedd0896ef8e544831b839fb328ffc5a2a2d5
Carlos Sousa <csousa90@gmail.com>
2021-04-06 21:57:38 +0200

    Added keepass2 instalation.

--- xubuntuDaily/001deploy.sh
+++ xubuntuDaily/001deploy.sh
@@ -10,11 +10,12 @@ echo "::  Installing common dependencies"
 sudo apt install software-properties-common apt-transport-https wget -y &> /dev/null
 
 echo "::  Adding Atom to thrusted sources"
-wget -q https://packagecloud.io/AtomEditor/atom/gpgkey -O- | sudo apt-key add - &> /dev/null
-sudo add-apt-repository "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" &> /dev/null
+wget -q https://packagecloud.io/AtomEditor/atom/gpgkey -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main"
 
-echo "::  Installing Atom"
+echo "::  Installing Atom, Keepass2"
 sudo apt install atom -y &> /dev/null
+sudo apt install keepass2 -y &> /dev/null
 
 
 echo "::  Installing: htops, kvm and it's utilities..."
@@ -24,6 +25,10 @@ sudo apt install virt-manager -y &> /dev/null
 sudo apt install libvirt-daemon-system -y &> /dev/null
 sudo apt install libvirt-clients -y &> /dev/null
 sudo apt install bridge-utils -y &> /dev/null
+
+echo "Adding user < ${USER} > to VM groups..."
+sudo adduser ${USER} libvirt
+sudo adduser ${USER} kvm
 
 echo "::  Removing Bloatware..."
 sudo apt remove gnome-mines -y &> /dev/null
