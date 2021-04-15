efcde8d907040d69ecbe123da69c428b7c6e20a6
Carlos Sousa <csousa90@gmail.com>
2021-04-15 02:16:14 +0200

    Fixed autoremove

--- xubuntuDaily/010update.sh
+++ xubuntuDaily/010update.sh
@@ -5,7 +5,7 @@ echo "::  Upgrading"
 sudo apt upgrade -y &> /dev/null &&
 echo "::  Removing Old Packages"
 sudo apt autoclean -y &> /dev/null
-sudo apt auto-remove -y &> /dev/null
+sudo apt autoremove -y &> /dev/null
 end=`date +%s`
 runtime=$((end-start))
 echo "::  Total Update Time: ${runtime} seconds!"
