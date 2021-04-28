b0046a59c05e76138d548e4819a6858295dd1840
Carlos Sousa <csousa90@gmail.com>
2021-04-28 09:52:20 +0200

    Added small preview of packages being updated.

--- xubuntuDaily/010update.sh
+++ xubuntuDaily/010update.sh
@@ -1,11 +1,12 @@
 echo "::  Updating"
 start=`date +%s`
 sudo apt update -y &> /dev/null &&
-echo "::  Upgrading"
-sudo apt upgrade -y &> /dev/null &&
+echo "::  Upgrading the following:"
+sudo apt list --upgradable
+sudo apt upgrade -y
 echo "::  Removing Old Packages"
-sudo apt autoclean -y &> /dev/null
-sudo apt autoremove -y &> /dev/null
+sudo apt autoclean -y
+sudo apt autoremove -y
 end=`date +%s`
 runtime=$((end-start))
 echo "::  Total Update Time: ${runtime} seconds!"
