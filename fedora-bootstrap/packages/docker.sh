898cec87579d2d6d8e49c985c6b67ce5ece1c9f3
Carlos Sousa <me@carlossousa.tech>
2025-05-19 15:45:15 +0200

    add user to docker, libvirt on setup + setup nvidia container

--- fedora-bootstrap/packages/docker.sh
+++ fedora-bootstrap/packages/docker.sh
@@ -16,3 +16,25 @@ fi
 
 echo "${INSTALLING_PRE_TEXT} docker, docker-compose"
 eval "sudo dnf install docker docker-compose -y $OUTPUT_CONTROL"
+
+
+
+
+if [ -z "$MAIN_USER" ]; then
+    echo "MAIN_USER is not set. Skipping adding user to docker group"
+else
+    echo "Adding ${MAIN_USER} to docker group"
+    eval "sudo usermod -aG docker $MAIN_USER"
+fi
+
+if [ "$SETUP_NVIDIA" != true ]; then
+    echo "${SKIPPING_PRE_TEXT} nvidia container toolkit. SETUP_NVIDIA is not true"
+else
+    echo "Adding nvidia-container-toolkit repo"
+    eval "curl -s -L https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo | sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo"
+
+    echo "${INSTALLING_PRE_TEXT} nvidia-container-tookit"
+    eval "sudo dnf install -y nvidia-container-toolkit -y $OUTPUT_CONTROL"
+    eval "sudo nvidia-ctk runtime configure --runtime=docker"
+    eval "sudo systemctl restart docker"
+fi
--- fedora-bootstrap/packages/qemu-kvm.sh
+++ fedora-bootstrap/packages/qemu-kvm.sh
@@ -6,3 +6,12 @@ fi
 
 echo "${INSTALLING_PRE_TEXT} qemu-kvm virt-manager bridge-utils libvirt"
 eval "sudo dnf install qemu-kvm virt-manager libvirt bridge-utils -y $OUTPUT_CONTROL"
+
+
+if [ -z "$MAIN_USER" ]; then
+    "MAIN_USER is not set. Skipping adding user to libvirt"
+else
+    echo "Adding ${MAIN_USER} to libvirt"
+    eval "sudo usermod -aG libvirt $MAIN_USER"
+fi
+
